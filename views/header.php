8fa6c3b5a2513c1ef56b1d5786f4c56eb6495ca0
millerbright <sangquangnqs@gmail.com>
2024-04-21 10:23:04 +0700

    headerfooter

--- views/header.php
+++ views/header.php
@@ -1,5 +1,56 @@
-<div name="header" class="flex lg:max-w-screen-xl mx-auto bg-rose-100">
+<!-- <div name="header" class="flex lg:max-w-screen-xl mx-auto bg-rose-100">
     <div>logo</div>
     <div class="mx-auto">login</div>
     <div>icon</div>
-</div>
+</div> -->
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equip="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="../assets/img/favicon.ico" sizes="16x16">
+        <script src="https://kit.fontawesome.com/20d7a8279b.js" crossorigin="anonymous"></script>
+        <title>Sách hay</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <link href="../assets/css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <div name="header" class="container lg:max-w-screen-xl mx-auto bg-violet-50 lg:px-16 sm:px-2 md:px-10">
+            <nav class="flex justify-between items-center py-3">
+                <div class="w-20">
+                    <img class=" border-r border-gray-400 px-3 cursor-pointer hover-fade" src="../assets/img/fahasa.png">
+                </div>
+
+                <div class="px-2 font-semibold uppercase hover:text-rose-700">
+                    <p>Fahasa</p>
+                    <p>shop</p>
+                </div>
+
+                <div class="mx-auto">
+                    <div class="relative w-full sm:w-64 md:w-96 xl:w-96">
+                        <input type="text" class="hidden xl:block w-full py-2 pl-8 pr-4 rounded-lg border-2 border-gray-300 focus:border-blue-500 focus:outline-none" placeholder="Search">
+
+                        <div class="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-6 w-6 text-gray-400">
+                                <path fill="currentColor" d="M16.293 15.707l3.999 3.999a1 1 0 0 1-1.414 1.414l-3.999-3.999a8 8 0 1 1 1.414-1.414zM10 16a6 6 0 1 0 0-12a6 6 0 0 0 0 12z"/>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="flex justify-end">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="h-10 w-20 border-r border-violet-300 px-1 cursor-pointer hover-fade">
+                        <path fill="#8a79be" d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z"/>
+                    </svg>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" class="h-10 w-20 cursor-pointer hover-fade">
+                        <path fill="#8a79be" d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/>
+                    </svg>
+
+                </div>
+            </nav>
+        </div>
+    </body>
+</html>
