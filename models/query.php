e79b72533eecb31df1338e968ca82a44cd8e6fce
DanteBartel <dantedbartel@gmail.com>
2024-04-26 17:52:48 +0700

    240426 1752; Nhan; fix model query method

--- models/query.php
+++ models/query.php
@@ -22,16 +22,19 @@ function query($method, $api_file, $data = []) {
             break;
         case 'POST':
             curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
             break;
         case 'PUT':
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
             break;
         case 'DELETE':
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
             if (!empty($data)) {
-                curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+                curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
             }
             break;
         default:
