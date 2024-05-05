c87429dfa616ae9a1e0976d38bb7e89946348c16
millerbright <sangquangnqs@gmail.com>
2024-05-05 22:01:13 +0700

    240505 2202; Sang; update account_admin.php & books_admin.php

--- views/accounts_admin.php
+++ views/accounts_admin.php
@@ -27,24 +27,26 @@
     ?>
     <hr class="border-t border-gray-300 my-4">
 
-    <div class="w-screen bg-gray-400">
-        <div class="mx-auto w-[1000px] p-8 bg-gray-100">
-            <h1 class="text-2xl">
-                Read Accounts
-            </h1>
-            <hr class="border-t-2 border-gray-300 my-4">
+    <div class="w-screen">
+        <div class="mx-auto lg:max-w-screen-xl p-2 md:p-8 bg-violet-100 rounded-3xl">
+            <div class="shadow-md border-2 border-gray-900 w-96 mx-auto rounded-2xl bg-red-600 bg-opacity-70 py-1">
+                <h1 class="text-2xl mx-auto text-center uppercase font-extrabold text-slate-100">
+                    Read Accounts
+                </h1>
+            </div>
+            <!-- <hr class="border-t-2 border-gray-300 my-4"> -->
             <!-- <form action="index.php" method="GET">
                 <input type="hidden" name="action" value="books"></input>
                 <input type="hidden" name="verb" value="new"></input>
                 <input type="submit" value="Create New Book" class="bg-blue-700 hover:bg-blue-500 p-1 border-solid border-2 border-black text-white font-bold rounded"></input>
             </form> -->
-            <table class="mt-4 min-w-full">
+            <table class="mt-4 min-w-full mr-2">
                 <thead>
                     <tr>
-                        <th class="text-left min-w-12 border-2 border-gray-300">ID</th>
-                        <th class="text-left min-w-32 border-2 border-gray-300">Username</th>
-                        <th class="text-left border-2 border-gray-300">Email</th>
-                        <th class="text-left min-w-40 border-2 border-gray-300">Action</th>
+                        <th class="text-center min-w-12 border-2 border-gray-400">ID</th>
+                        <th class="text-center min-w-12 md:min-w-32 border-2 border-gray-400">Username</th>
+                        <th class="text-center min-w-16 border-2 border-gray-400">Email</th>
+                        <th class="text-center min-w-16 border-2 border-gray-400">Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -53,10 +55,10 @@
                             foreach ($accounts as $account) {
                     ?>
                                 <tr>
-                                    <td class="border-2 border-gray-300"><?php echo $account->id; ?></td>
-                                    <td class="border-2 border-gray-300"><?php echo $account->username; ?></td>
-                                    <td class="border-2 border-gray-300"><?php echo $account->email; ?></td>
-                                    <td class="border-2 border-gray-300 align-top">
+                                    <td class="text-center border-2 border-gray-400"><?php echo $account->id; ?></td>
+                                    <td class="text-center border-2 border-gray-400"><?php echo $account->username; ?></td>
+                                    <td class="text-center border-2 border-gray-400"><?php echo $account->email; ?></td>
+                                    <td class="text-center border-2 border-gray-400 align-top">
                                         <div class="flex">
                                             <!-- <form action="index.php" method="get">
                                                 <input type="hidden" name="action" value="accounts">
@@ -64,7 +66,7 @@
                                                 <input type="hidden" name="id" value="<?php echo $account->id; ?>">
                                                 <input type="submit" value="Edit" class="bg-blue-700 hover:bg-blue-500 p-1 border-solid border-2 border-black text-white font-bold rounded"></input>
                                             </form> -->
-                                            <form action="index.php?action=accounts&verb=destroy" method="post">
+                                            <form action="index.php?action=accounts&verb=destroy" method="post" class="mx-auto">
                                                 <input type="hidden" name="id" value="<?php echo $account->id; ?>">
                                                 <input type="submit" value="Delete" class="bg-rose-700 hover:bg-rose-500 p-1 border-solid border-2 border-black text-white font-bold rounded"></input>
                                             </form>
--- views/books_admin.php
+++ views/books_admin.php
@@ -42,25 +42,27 @@
     ?>
     <hr class="border-t border-gray-300 my-4">
 
-    <div class="w-screen bg-gray-400">
-        <div class="mx-auto w-[1000px] p-8 bg-gray-100">
-            <h1 class="text-2xl">
-                Read Books
-            </h1>
+    <div class="w-screen">
+        <div class="mx-auto lg:max-w-screen-xl p-2 pr-2 md:p-8 bg-violet-100 rounded-3xl">
+            <div class="shadow-md border-2 border-gray-900 w-96 mx-auto rounded-2xl bg-red-600 bg-opacity-70 py-1">
+                <h1 class="text-2xl  mx-auto text-center uppercase font-extrabold text-slate-100">
+                    Read Books
+                </h1>
+            </div>
             <hr class="border-t-2 border-gray-300 my-4">
             <form action="index.php" method="GET">
                 <input type="hidden" name="action" value="books"></input>
                 <input type="hidden" name="verb" value="new"></input>
-                <input type="submit" value="Create New Book" class="bg-blue-700 hover:bg-blue-500 p-1 border-solid border-2 border-black text-white font-bold rounded"></input>
+                <input type="submit" value="Create New Book" class="bg-violet-900 bg-opacity-70 hover:bg-violet-700 p-1 border-solid border-2 border-black text-white font-bold rounded"></input>
             </form>
-            <table class="mt-4 min-w-full">
+            <table class="mt-4 min-w-full mr-2">
                 <thead>
                     <tr>
-                        <th class="text-left min-w-12 border-2 border-gray-300">ID</th>
-                        <th class="text-left min-w-32 border-2 border-gray-300">Name</th>
-                        <th class="text-left border-2 border-gray-300">Description</th>
-                        <th class="text-left min-w-32 border-2 border-gray-300">Price</th>
-                        <th class="text-left min-w-40 border-2 border-gray-300">Action</th>
+                        <th class="text-center md:min-w-12 border-2 border-gray-400">ID</th>
+                        <th class="text-center md:min-w-32 border-2 border-gray-400">Name</th>
+                        <th class="text-center border-2 border-gray-400">Description</th>
+                        <th class="text-center md:min-w-32 border-2 border-gray-400">Price</th>
+                        <th class="text-center md:min-w-40 border-2 border-gray-400">Action</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -69,17 +71,17 @@
                             foreach ($books as $book) {
                     ?>
                                 <tr>
-                                    <td class="border-2 border-gray-300"><?php echo $book->id; ?></td>
-                                    <td class="border-2 border-gray-300"><?php echo $book->name; ?></td>
-                                    <td class="border-2 border-gray-300"><?php echo $book->description; ?></td>
-                                    <td class="border-2 border-gray-300"><?php echo $book->price; ?> VND</td>
-                                    <td class="border-2 border-gray-300 align-top">
-                                        <div class="flex">
-                                            <form action="index.php" method="get">
+                                    <td class="text-center border-2 border-gray-400"><?php echo $book->id; ?></td>
+                                    <td class="text-center border-2 border-gray-400"><?php echo $book->name; ?></td>
+                                    <td class="border-2 border-gray-400"><?php echo $book->description; ?></td>
+                                    <td class="text-center border-2 border-gray-400 font-semibold"><?php echo $book->price; ?> VND</td>
+                                    <td class="border-2 border-gray-400 align-top">
+                                        <div class="flex py-10 md:pl-6">
+                                            <form action="index.php" method="get" class="pr-2">
                                                 <input type="hidden" name="action" value="books">
                                                 <input type="hidden" name="verb" value="edit">
                                                 <input type="hidden" name="id" value="<?php echo $book->id; ?>">
-                                                <input type="submit" value="Edit" class="bg-blue-700 hover:bg-blue-500 p-1 border-solid border-2 border-black text-white font-bold rounded"></input>
+                                                <input type="submit" value="Edit" class="bg-violet-900 bg-opacity-50 hover:bg-violet-700 p-1 border-solid border-2 border-black text-white font-bold rounded"></input>
                                             </form>
                                             <form action="index.php?action=books&verb=destroy" method="post">
                                                 <input type="hidden" name="id" value="<?php echo $book->id; ?>">
