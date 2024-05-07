d8a5264fae037b747c243818f56ccdd1e59fbfe3
HiVuong <vuong.vovhv02bk@hcmut.edu.vn>
2024-05-08 01:34:27 +0700

    css fixing

--- views/home.php
+++ views/home.php
@@ -25,7 +25,7 @@ $ids = [];
 </head>
 
 <body>
-	<!-- TODO: Fix the header problem -->
+
 	<?php require 'views/header.php'; ?>
 	<hr class="border-t border-gray-300">
 	<div class="w-full flex justify-center">
@@ -106,51 +106,13 @@ $ids = [];
 					'xl' => ['text-3xl', 'my-6'],
 				]);
 				?>">You must buy it now</div>
+
 				<!-- Carousel of cards -->
-
-
 				<div class="flex overflow-x-scroll">
-					<!-- Card -->
-					<div class="bg-white border border-gray-200 rounded-lg shadow mx-4 flex-none<?php
-					parseResponsiveCSS([
-						'' => ['w-3/5', 'min-w-3/5'],
-						'md' => ['w-1/4', 'min-w-1/4'],
-						'xl' => ['w-1/5', 'min-w-1/5']
-					]);
-					?>">
-						<!-- Book cover -->
-						<a href="#">
-							<img class="rounded-t-lg" src="assets/img/alt.jpg" alt="" />
-						</a>
 
-						<!-- Book info -->
-						<div class="p-5">
-							<a href="#">
-								<h5 class="mb-2 font-bold tracking-tight text-gray-900 text-xl
-								">The vanishing act</h5>
-							</a>
-							<p class="mb-3 font-normal text-gray-500 text-base">Tori Dunlap</p>
-							<div class="flex justify-between items-center mb-5">
-								<span class="text-lg font-bold">$20.00</span> <!-- Price -->
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-auto"> <!-- SVG -->
-									<path fill="white" stroke="purple" stroke-width="20"
-										d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
-								</svg>
-							</div>
-
-							<a href="#"
-								class="inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
-								<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="w-4 h-auto mr-2">
-									<path fill="white"
-										d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96zM252 160c0 11 9 20 20 20h44v44c0 11 9 20 20 20s20-9 20-20V180h44c11 0 20-9 20-20s-9-20-20-20H356V96c0-11-9-20-20-20s-20 9-20 20v44H272c-11 0-20 9-20 20z" />
-								</svg>
-								Add to cart
-							</a>
-						</div>
-					</div>
-
+					<!-- Parsing book cards -->
 					<?php foreach ($books as $book): ?>
-						<div class="bg-white border border-gray-200 rounded-lg shadow mx-4 flex-none<?php
+						<div class="bg-white flex flex-col border border-gray-200 rounded-lg shadow mx-4 flex-none<?php
 						parseResponsiveCSS([
 							'' => ['w-3/5'],
 							'md' => ['w-1/4'],
@@ -158,38 +120,46 @@ $ids = [];
 						]);
 						?>">
 							<!-- Book cover -->
-							<a href="index.php?action=book&id=<?php echo $book->id ?>">
-								<img class="rounded-t-lg" src="<?php echo $book->image; ?>" alt="" />
-							</a>
+							<div class="flex items-center justify-center w-auto h-3/5 rounded-lg overflow-hidden">
+								<a href="index.php?action=book&id=<?php echo $book->id ?>">
+									<img class="object-contain" src="<?php echo $book->image; ?>" alt="Book cover" />
+								</a>
+							</div>
 
 							<!-- Book info -->
-							<div class="p-5">
-								<a href="#">
-									<h5 class="mb-2 font-bold tracking-tight text-gray-900 text-xl">
-										<?php echo $book->name; ?>
-									</h5>
-								</a>
-								<p class="mb-3 font-normal text-gray-500 text-base"><?php echo $book->author; ?></p>
-								<div class="flex justify-between items-center mb-5">
-									<span class="text-lg font-bold">$<?php echo $book->price; ?></span> <!-- Price -->
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-auto"> <!-- SVG -->
-										<path fill="white" stroke="purple" stroke-width="20"
-											d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
-									</svg>
+							<div class="flex-grow flex flex-col justify-between p-5">
+								<div>
+									<a href="#">
+										<h5 class="mb-2 font-bold tracking-tight text-gray-900 text-xl">
+											<?php echo $book->name; ?>
+										</h5>
+									</a>
+									<p class="mb-3 font-normal text-gray-500 text-base"><?php echo $book->author; ?></p>
 								</div>
 
-								<a href="#"
-									class="inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
-									<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="w-4 h-auto mr-2">
-										<path fill="white"
-											d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96zM252 160c0 11 9 20 20 20h44v44c0 11 9 20 20 20s20-9 20-20V180h44c11 0 20-9 20-20s-9-20-20-20H356V96c0-11-9-20-20-20s-20 9-20 20v44H272c-11 0-20 9-20 20z" />
-									</svg>
-									Add to cart
-								</a>
+								<div>
+									<div class="flex justify-between items-center mb-5">
+										<span class="text-lg font-bold">$<?php echo $book->price; ?></span> <!-- Price -->
+										<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="w-5 h-auto"> <!-- SVG -->
+											<path fill="white" stroke="purple" stroke-width="20"
+												d="M47.6 300.4L228.3 469.1c7.5 7 17.4 10.9 27.7 10.9s20.2-3.9 27.7-10.9L464.4 300.4c30.4-28.3 47.6-68 47.6-109.5v-5.8c0-69.9-50.5-129.5-119.4-141C347 36.5 300.6 51.4 268 84L256 96 244 84c-32.6-32.6-79-47.5-124.6-39.9C50.5 55.6 0 115.2 0 185.1v5.8c0 41.5 17.2 81.2 47.6 109.5z" />
+										</svg>
+									</div>
+
+									<div class="flex items-center">
+										<a href="#"
+											class="inline-flex justify-center items-center px-3 py-2 text-sm font-medium text-center text-white bg-purple-700 rounded-lg hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+											<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="w-4 h-auto mr-2">
+												<path fill="white"
+													d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96zM252 160c0 11 9 20 20 20h44v44c0 11 9 20 20 20s20-9 20-20V180h44c11 0 20-9 20-20s-9-20-20-20H356V96c0-11-9-20-20-20s-20 9-20 20v44H272c-11 0-20 9-20 20z" />
+											</svg>
+											Add to cart
+										</a>
+									</div>
+								</div>
 							</div>
 						</div>
 					<?php endforeach; ?>
-
 				</div>
 
 			</div>
